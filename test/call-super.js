fd9d939bbf8565d86c6504709a665c5bb05fb469
Nick Acker <nick.acker@mindflash.com>
2017-03-08 13:45:53 -0800

    Improve call-super tests

--- test/call-super.js
+++ test/call-super.js
@@ -7,32 +7,55 @@ import objectionTimestamp from '../';
 
 const knex = Knex({});
 objection.Model.knex(knex);
-objection.Model.prototype.$beforeInsert = function() {
-  return Promise.resolve()
-  .then(() => {
-    this.superInsertProperty = true;
-  });
-}
-objection.Model.prototype.$beforeUpdate = function() {
-  return Promise.resolve()
-  .then(() => {
-    this.superUpdateProperty = true;
-  });
-}
+const origModelClass = objection.Model;
 
 objectionTimestamp.register(objection);
 
 class Model extends objection.Model {
 }
 
-test('Call super.$beforeInsert', async t => {
+test('Call super.$beforeInsert (synchronous)', async t => {
+  origModelClass.prototype.$beforeInsert = function() {
+    this.foo = true;
+  };
+
   const model = new Model();
   await model.$beforeInsert();
-  t.is(model.superInsertProperty, true);
+  t.is(model.foo, true);
 });
 
-test('Call super.$beforeUpdate', async t => {
+test('Call super.$beforeInsert (promise)', async t => {
+  origModelClass.prototype.$beforeInsert = function() {
+    return Promise.resolve()
+    .then(() => {
+      this.foo = true;
+    });
+  };
+
+  const model = new Model();
+  await model.$beforeInsert();
+  t.is(model.foo, true);
+});
+
+test('Call super.$beforeUpdate (synchronous)', async t => {
+  origModelClass.prototype.$beforeUpdate = function() {
+    this.foo = true;
+  };
+
   const model = new Model();
   await model.$beforeUpdate();
-  t.is(model.superUpdateProperty, true);
+  t.is(model.foo, true);
+});
+
+test('Call super.$beforeUpdate (promise)', async t => {
+  origModelClass.prototype.$beforeUpdate = function() {
+    return Promise.resolve()
+    .then(() => {
+      this.foo = true;
+    });
+  };
+
+  const model = new Model();
+  await model.$beforeUpdate();
+  t.is(model.foo, true);
 });
